1ee04721d5be7cfaf67680cb188af5ce0032a005
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-01-05 22:39:38 -0600

    Add comments

--- app.R
+++ app.R
@@ -1,17 +1,27 @@
+# import Shiny library
 library(shiny)
-
+# define UI component
 ui <- fluidPage(titlePanel("Date and Time in R Shiny"), sidebarLayout(sidebarPanel(textInput("year",
-    "Year", value = "2022"), sliderInput("month", "Month", min = 1,
-    max = 12, value = 1, step = 1), sliderInput("day", "Day",
-    min = 1, max = 31, value = 1, step = 1), sliderInput("hour",
-    "Hour", min = 0, max = 23, value = 0, step = 1), sliderInput("minute",
-    "Minute", min = 0, max = 59, value = 0, step = 1), sliderInput("second",
-    "Second", min = 0, max = 59, value = 0, step = 1), sliderInput("millisecond",
-    "Millisecond", min = 0, max = 999, value = 0, step = 1)),
-    mainPanel(textOutput("date_time"))))
-
+    "Year", value = "2022")  #year input
+, sliderInput("month",
+    "Month", min = 1, max = 12, value = 1, step = 1)  #month input
+,
+    sliderInput("day", "Day", min = 1, max = 31, value = 1, step = 1)  #day of month input
+, sliderInput("hour", "Hour", min = 0,
+        max = 23, value = 0, step = 1)  #hour input
+, sliderInput("minute",
+        "Minute", min = 0, max = 59, value = 0, step = 1)  #minute input
+, sliderInput("second", "Second", min = 0,
+        max = 59, value = 0, step = 1)  #second input
+, sliderInput("millisecond",
+        "Millisecond", min = 0, max = 999, value = 0, step = 1)  #millisecond input
+), mainPanel(textOutput("date_time")  #date and time output
+)))
+# define server component
 server <- function(input, output) {
     date_time <- reactive({
+        # create date and time object from full date and
+        # time string
         as.POSIXct(sprintf("%04d-%02d-%02d %02d:%02d:%02d.%03d",
             as.integer(input$year), as.integer(input$month),
             as.integer(input$day), as.integer(input$hour), as.integer(input$minute),
@@ -19,8 +29,9 @@ server <- function(input, output) {
             tz = "UTC")
     })
     output$date_time <- renderText({
+        # date and time output
         paste("Date and Time:", date_time())
     })
 }
-
+# run the Shiny web app server
 shinyApp(ui = ui, server = server)
