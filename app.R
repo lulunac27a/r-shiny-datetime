19df3c60870bc1f386cc83cb618dc51e652b5889
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-01-06 19:37:47 -0600

    Add more date and time formats

--- app.R
+++ app.R
@@ -15,7 +15,11 @@ ui <- fluidPage(titlePanel("Date and Time in R Shiny"), sidebarLayout(sidebarPan
         max = 59, value = 0, step = 1)  #second input
 , sliderInput("millisecond",
         "Millisecond", min = 0, max = 999, value = 0, step = 1)  #millisecond input
-), mainPanel(textOutput("date_time")  #date and time output
+), mainPanel(textOutput("iso_format")  #ISO format output
+, textOutput("us_format")  #US format output
+,
+    textOutput("eu_format")  #EU format output
+, textOutput("date_time")  #date and time output
 )))
 # define server component
 server <- function(input, output) {
@@ -31,6 +35,20 @@ server <- function(input, output) {
     output$date_time <- renderText({
         # date and time output
         paste("Date and Time:", date_time())
+    })
+    output$iso_format <- renderText({
+        # ISO format output
+        paste("ISO Format:", format(date_time(), "%Y-%m-%dT%H:%M:%OS3Z"))
+    })
+
+    output$us_format <- renderText({
+        # US format output
+        paste("US Format:", format(date_time(), "%m/%d/%Y %I:%M:%S %p"))
+    })
+
+    output$eu_format <- renderText({
+        # EU format output
+        paste("EU Format:", format(date_time(), "%d/%m/%Y %H:%M:%S"))
     })
 }
 # run the Shiny web app server
