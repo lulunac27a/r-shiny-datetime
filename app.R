db6a51b5a71697fd0466db090766ef909950ca6e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-01-07 16:47:26 -0600

    Add libridate package

--- app.R
+++ app.R
@@ -1,5 +1,7 @@
 # import Shiny library
 library(shiny)
+# import lubridate library
+library(lubridate)
 # define UI component
 ui <- fluidPage(titlePanel("Date and Time in R Shiny"), sidebarLayout(sidebarPanel(textInput("year",
     "Year", value = format(Sys.Date(), "%Y"))  #year input
@@ -18,11 +20,6 @@ ui <- fluidPage(titlePanel("Date and Time in R Shiny"), sidebarLayout(sidebarPan
         step = 1)  #second input
 , sliderInput("millisecond",
         "Millisecond", min = 0, max = 999, value = 0, step = 1)  #millisecond input
-, sliderInput("microsecond", "Microsecond",
-        min = 0, max = 999, value = 0, step = 1)  #microsecond input
-,
-    sliderInput("nanosecond", "Nanosecond", min = 0, max = 999,
-        value = 0, step = 1)  #nanosecond input
 ), mainPanel(textOutput("iso_format")  #ISO format output
 , textOutput("us_format")  #US format output
 ,
@@ -73,11 +70,10 @@ server <- function(input, output, session) {
     date_time <- reactive({
         # create date and time object from full date and
         # time string
-        as.POSIXct(sprintf("%04d-%02d-%02d %02d:%02d:%02d.%03d%03d%03d",
+        as.POSIXct(sprintf("%04d-%02d-%02d %02d:%02d:%02d.%03d",
             as.integer(input$year), as.integer(input$month),
             as.integer(input$day), as.integer(input$hour), as.integer(input$minute),
-            as.integer(input$second), as.integer(input$millisecond),
-            as.integer(input$microsecond), as.integer(input$nanosecond)),
+            as.integer(input$second), as.integer(input$millisecond)),
             tz = "UTC")
     })
     output$date_time <- renderText({
